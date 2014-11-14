5db4a0928e260a0458a7bd14ab174ee922bbb64c
tushar <tushar.behror@gmail.com>
2014-11-14 12:06:22 +0530

    modified:   login.php 	modified:   register.php 	modified:   temp.php

--- login.php
+++ login.php
@@ -22,9 +22,48 @@ echo "failed";
 }
 
 ?>
-	
+<<<<<<< HEAD
+<head>
+<style>
+        body
+        {
+          background-image: url("help.jpg");
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+          background-position: 0px 55px;
+         }
+         
+        </style>
+</head>
+<body>
+       <div class="container">
+  <div class="row" style="margin-top:50px; padding-right:300px">
+    <div class="col-md-4 col-md-offset-4">
+      <form method="POST" action="login.php" accept-charset="UTF-8" role="form" id="loginform" class="form-signin"><input name="_token" type="hidden" value="snIDVlxZSk7KEJjGcPcP9EmwfyY1lMyIuaU5s8ct">
+        <fieldset>
+            <h3 class="sign-up-title" style="color:dimgray;">Welcome back! sign in</h3>
+
+            <hr class="colorgraph">
+            <input type="text" class="form-control" id="loginid" name="loginid" placeholder="Username">
+            <input type="password" class="form-control" id="pass" name="pass" placeholder="Password">
+            <div class="checkbox">
+              <label><input name="remember" type="checkbox" value="Remember Me"> Remember Me</label>
+            </div>
+            <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+          </fieldset>
+        </form>
+    </div>
+    </div>
+</div>
+    </div>
+
+   <div>
+ <!--  <form class="form-horizontal" role="form" name="form" method="POST" action="login.php">
+=======
+  
   <form role="form" name="form" method="POST" action="login.php">
   
+>>>>>>> 55c9b593c905f2dff6d213f4e40ef384cdbd327d
   
   <div class="form-group">
     <label for="loginid">User Name</label>
@@ -34,6 +73,21 @@ echo "failed";
     <label for="pass1">Password</label>
     <input type="password" class="form-control" id="pass" name="pass" placeholder="Enter your Password">
   </div>
+<<<<<<< HEAD
+  <button type="submit" class="btn btn-default" onclick="return check()">Sign In</button>
+</form>
+ -->
+
+
+ <script text-align:center>
+ function check()
+ {
+
+ return true;
+ }
+ </script>
+</body>
+=======
   
   
 <button type="submit" class="btn btn-default" onclick="return check()">Sign In</button>
@@ -46,3 +100,4 @@ function check()
 return true;
 }
 </script>
+>>>>>>> 55c9b593c905f2dff6d213f4e40ef384cdbd327d
--- temp.php
+++ temp.php
@@ -6,11 +6,62 @@ include_once 'cn.php';
 
 <head>
 
+<<<<<<< HEAD
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+        <script src="js/bootstrap.js">
+        </script>
+        <link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+        
+    <style>
+
+    body {
+        padding-top: 10px;
+        padding-left: ;
+        
+        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+        background-color:black;
+    }
+    </style>
+
+=======
+>>>>>>> 55c9b593c905f2dff6d213f4e40ef384cdbd327d
 
 </head>
 
 <body>
 
+<<<<<<< HEAD
+        <div class="container" style="background-color:; "> 
+      <div class="custom-menu">
+        <nav class="navbar" role="navigation">
+          <ul class="nav nav-pills">
+            <li class="active col-md-2 text-center"><a href="login.php">Sign in</a></li>
+             <li class="active col-md-2 text-center"><a href="register.php">Sign up</a></li>
+              <li class="active col-md-2 text-center"><a href="index.html">About us</a></li>
+            
+              
+            </li>
+        </nav>
+      </div>  
+    </div>
+
+<h1 style="text-align:center"></h1>
+ <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+=======
+>>>>>>> 55c9b593c905f2dff6d213f4e40ef384cdbd327d
 
 </body>
 
