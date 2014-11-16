045464de0d3f3c60d5b6cf0a1883cfd3e21424db
tushar <tushar.behror@gmail.com>
2014-11-16 16:28:00 +0530

    modified:   index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include_once 'temp.php';
 <?php
 
 ?>
+
 <head>
 <style>
 body{
@@ -14,50 +15,55 @@ body{
 	background-size: 1350px 250px;
 }
  
-.solid {
-    background: dimgray;
+.row {
+    background-color: lightgray;
+   
+ }
+ .active{
+ 	font-size: 35px;
  }
 
 </style>
 </head>
-<p style="text-align:center;font-size: 40px; color:red"><bold>New Arrivals!!</bold></p>
 
+<body>
+<p style="text-align:center;font-size: 40px; color:red"><bold>New Arrivals!!</bold></p>
 <br><br><br><br>
+
 <div style="padding-left:35%;">
 <form class="navbar-form navbar-left" role="search" method="POST" action="ads.php" >
 
-  <div class="form-group">
-
+    <div class="form-group">
     <input type="text" class="form-control" id="search" name="search" placeholder="Enter Item To Search">
-  </div>
-  <button type="submit" class="btn btn-default" onclick="return check()">Search</button>
+    </div>
+
+    <button type="submit" class="btn btn-default" onclick="return check()">Search</button>
 </form>
 <br>
 <h5 ><a href="adpost.php"><strong>Post an Advertisement</strong></a></h5>
 </div>
 
 <br>
-<!-- <form role="form" name="form" method="POST" action="ads.php">
-  
-  <div class="form-group">
-       <input type="text" class="form-control" id="search" name="search" placeholder="Enter Item To Search">
-<button type="submit" class="btn btn-default" onclick="return check()">Search</button>
-  </div>
-  </form> -->
-<h3 style="padding-left:40px; color:green"><strong>Categories</strong></h3>
-<div style="padding-right:1100px; padding-left:10px">
-<div class="solid" style="padding-left:30px">
-<?php
- $query=mysql_query("SELECT * FROM Category1 order by Category",$cn) or die(mysql_error());
- while($fet = mysql_fetch_array($query))
- {
-        echo "<a href='ads.php?cat=".$fet['ID']."'>".$fet['Category']."</a></br>";
-    }
-?>
 
-</div>
+<div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active">Category<span class="sr-only">(current)</span></li><br>
+            <li style="font-size:20px"> <?php
+            $query=mysql_query("SELECT * FROM Category1 order by Category",$cn) or die(mysql_error());
+            while($fet = mysql_fetch_array($query))
+            {
+                  echo "<a href='ads.php?cat=".$fet['ID']."'>".$fet['Category']."</a>";
+            }
+           ?></li>
+          </ul>
+        </div>
+      </div>
 </div>
 
+
+</body>
 <script>
 function check()
 {
