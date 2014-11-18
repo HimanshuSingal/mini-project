5f0951c58b1bcdedba5643ee0c96e05cca632ec9
Himanshu Singal <singal_himanshu@yahoo.co.in>
2014-11-18 13:22:36 +0530

    number limit validated

--- startend.php
+++ startend.php
@@ -3,8 +3,10 @@ ob_start();
 session_start();
 if(isset($_POST['start']) && isset($_POST['end']))
 {
+if($_POST['start']>0 && $_POST['end']>=$_POST['start']){
 echo $_SESSION['start']=$_POST['start']-1;
 	echo $_SESSION['end']=$_POST['end']-$_POST['start']+1;
+}
 header("Location:index.php");
 }
 ?>
