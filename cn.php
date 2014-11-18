a098a5da23b813442122f505cc48428d342d0fbd
tushar <tushar.behror@gmail.com>
2014-11-18 11:35:20 +0530

    new file:   adphotos/4.jpg 	new file:   adphotos/5.jpg 	modified:   blockads.php 	modified:   cn.php 	modified:   login.php 	modified:   temp.php 	modified:   tempadmin.php

--- cn.php
+++ cn.php
@@ -2,7 +2,7 @@
 /* To be configured during installation */
 $db_hostname = 'localhost';
 $db_username = 'root';
-$db_password = 'shyam';
+$db_password = 'mysql';
 $database = 'mini_project';
 $cn=mysql_connect($db_hostname, $db_username, $db_password);
 mysql_select_db($database,$cn);
